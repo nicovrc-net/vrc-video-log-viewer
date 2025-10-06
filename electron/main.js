c0a79a4a4c8d5687b2b3ab15a0596cf7fe061be0
Serie <120567699+RearAlice@users.noreply.github.com>
2025-10-06 20:25:01 +0900

    fix bug

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,10 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// __dirname の代替
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
@@ -15,7 +20,7 @@ function createWindow() {
   mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
